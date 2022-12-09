3634558caf57358b512fa95071a752dac9e98940
Esmay <Molinaesme729@gmail.com>
2022-12-09 10:38:35 -0700

    added food

--- collections/ViewController.swift
+++ collections/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tod: UITextField!
+    var myArrayOfFood = ["Butter", "Lemons", "Salad"]
+     
+    @IBOutlet weak var joe: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func sam(_ sender: Any) {
+        var stacy = tod.text ?? ""
+        myArrayOfFood.append(stacy)
+        var output = ""
+        
+        for veggie in 0...3 {
+            output += "\(myArrayOfFood[veggie])\n"
+        }
+        joe.text = output
+        
+        
 
+        
+    }
+    
 }
 
